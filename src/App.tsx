4c0b7355b7589c4e743b6a4ea07ffd135af77994
Pourya Daneshvar <pourya@daneshvar.me>
2021-12-22 15:55:14 +0330

    Session 04

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Item from './Item'
@@ -22,8 +22,33 @@ const data = [
 ]
 
 function App() {
+  const [state, setState] = useState<number>(1)
+  const [secondState, setSecondState] = useState<number>(1)
+  const [thirdState, setThirdState] = useState<{ a: number, b: number }>({
+    a: 1,
+    b: 100
+  })
+
+  useEffect(() => {
+    console.log('Change')
+  }, [state, secondState])
+
+
+  const counter1 = () => {
+
+    setState(state + 1);
+  }
+
+  const counter2 = () => {
+
+    setSecondState(secondState + 1)
+  }
+
   return (
     <div className="app">
+      <a onClick={counter1}>Click to count 1</a>
+      <a onClick={counter2}>Click to count 2</a>
+      <h1 className="timer">{state} {secondState} </h1>
       {
         data.map((data, index) =>
           <Item {...data} key={index} />
