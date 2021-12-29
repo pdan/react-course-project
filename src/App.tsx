c7447648d24e3aadaeb4a8d3a2ee48fd8666ac9c
Pourya Daneshvar <pourya@daneshvar.me>
2021-12-29 11:16:31 +0330

    API request

--- src/App.tsx
+++ src/App.tsx
@@ -1,54 +1,71 @@
 import React, { LegacyRef, MutableRefObject, Ref, useEffect, useRef, useState } from 'react';
+import { Form, Input, Button, Select } from 'antd';
 import logo from './logo.svg';
+import { ColumnsType } from 'antd/lib/table/Table'
 import './App.css';
-import Item from './Item'
-import { Button } from 'antd';
-import { ButtonHTMLType } from 'antd/lib/button/button';
-import { FormInstance } from 'rc-field-form';
+import { Server } from 'http';
+import axios from 'axios'
 
-const data = [
-  {
-    name: 'Atieh', description: 'A blog post',
-    time: 'Today at 5:00', image: '/assets/images/02.png'
-  },
 
-  {
-    name: 'Parisa', description: 'Hello world',
-    time: 'Today at 5:00', image: '/assets/images/03.png'
-  },
 
-  {
-    name: 'Sina', description: 'A Linux blog',
-    time: 'Today at 5:00', image: '/assets/images/01.png'
-  }
-]
-
-interface UserSignin {
+type FormResult = {
+  name: string
   username: string
-  password: string
+}
+
+type ServerData = {
+  base: string
+  clouds: any
+  cod: number
+  coord: any
+  dt: number
+  id: number
+  main: {
+    feels_like: number
+    humidity: number
+    pressure: number
+    temp: number
+    temp_max: number
+    temp_min: number
+  }
+  name: string
+  sys: any
+  timezone: number
+  visibility: number
+  weather: any[]
+  wind: any
 }
 
 function App() {
-  const form = React.createRef<HTMLFormElement>()
+  const [ServerData, SetServerData] = useState<ServerData>()
+  const headers = new Headers()
+  headers.append('Content-Type', 'application/json')
+  useEffect(() => {
+    (async () => {
+      const weather = await axios.get<ServerData>('https://api.openweathermap.org/data/2.5/weather?q=tehran')
+      const weather2 = await axios.request<ServerData>({
+        url: 'https://api.openweathermap.org/data/2.5/weather?q=tehran',
+        headers: {
+          'Authorization':''
+        }
+      })
 
-
-  const signin = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault()
-    console.log(
-      e.currentTarget.username.value,
-      e.currentTarget.password.value
-    )
-  }
-
+      
+      SetServerData(weather.data)
+    })()
+    
+    // weather.then((data) => {
+    //   return data.json()
+    // }).then(data => {
+    //   SetServerData(data)
+    //   console.log(data)
+    // })
+    
+  }, [ServerData])
 
   return (
     <div className="app">
-      <form style={{ display: 'flex', flexDirection: 'column' }} ref={form} onSubmit={signin}>
-        <input name="username" placeholder='username' />
-        <input name="password" type="password" placeholder='password' />
-
-        <button type={'submit'}>Submit</button>
-      </form>
+      <h1>{ServerData?.main?.temp}</h1>
     </div>
   );
 }
