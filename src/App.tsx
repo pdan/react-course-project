6a901471dab90a02ded3a501933413b3dfb9af5d
Pourya Daneshvar <pourya@daneshvar.me>
2022-01-15 09:55:53 +0330

    Create counter

--- src/App.tsx
+++ src/App.tsx
@@ -1,89 +1,24 @@
-import React, { LegacyRef, MutableRefObject, Ref, useEffect, useRef, useState, useContext, createContext, useReducer } from 'react';
-import { Form, Input, Button, Select } from 'antd';
-import logo from './logo.svg';
-import { ColumnsType } from 'antd/lib/table/Table'
-import './App.css';
-import { Server } from 'http';
-import axios from 'axios'
-import Item from './Item'
+import React, { useEffect } from 'react';
+import './App.css'
 
 
-const UserContext = createContext({})
-
-interface Props {
-  user:string
-}
-
-function Component1() {
-  const [user, setUser] = useState<string>('Rasad User')
-
-  // const [state, dispatch] = useReducer((state, action) => {
-  //   switch (action) {
-  //     case 'plus': 
-  //       return state + 1;
-  //     case 'cross': 
-  //       return state * 5;
-
-  //   }
-  // }, {})
-
-  // dispatch('plus')
-
-  
-  return (
-    <UserContext.Provider value={user}>
-      <h1>Component 1</h1>
-      
-      <Component2 />
-    </UserContext.Provider>
-  ) 
-}
-
-function Component2() {
-  return (
-    <>
-      <h1>Component 2</h1>
-      <Component3  />
-    </>
-  ) 
-}
-
-function Component3() {
-  return (
-    <>
-      <h1>Component 3</h1>
-      <Component4  />
-    </>
-  ) 
-}
-
-function Component4() {
-  const user = useContext(UserContext)
-
-  return (
-    <>
-      <h1>Component 4</h1>
-      <Component5  />
-    </>
-  ) 
-}
-
-function Component5() {
-  const user = useContext(UserContext)
-  return (
-    <>
-      <h1>Component 5</h1>
-      <h2>{`Hello ${user} again!`}</h2>
-    </>
-  ) 
-}
 
 function App() {
 
 
+
   return (
-    <Component1 />
-    
+    <div className='container'>
+      <div className='counter'>12</div>
+      <div className="control">
+        <button className='button decrement-button' onClick={() => { }}>
+          Decrement
+        </button>
+        <button className='button increment-button' onClick={() => { }}>
+          Increment
+        </button>
+      </div>
+    </div>
   );
 }
 
---
+++ src/mock.ts
@@ -0,0 +1,164 @@
+export const data = [
+    {
+        id: 1,
+        name: 'Chart #1',
+        intervalTime: 120,
+        position: {
+            x: 100,
+            y: 120
+        },
+        chartItems: [
+            {
+                id: 100,
+                name: 'Item #1'
+            },
+            {
+                id: 100,
+                name: 'Item #2'
+            },
+            {
+                id: 100,
+                name: 'Item #3'
+            },
+            {
+                id: 100,
+                name: 'Item #4'
+            }
+        ]
+    },
+    {
+        id: 2,
+        name: 'Chart #2',
+        intervalTime: 120,
+        position: {
+            x: 100,
+            y: 120
+        },
+        chartItems: [
+            {
+                id: 100,
+                name: 'Item #1'
+            },
+            {
+                id: 100,
+                name: 'Item #2'
+            },
+            {
+                id: 100,
+                name: 'Item #3'
+            },
+            {
+                id: 100,
+                name: 'Item #4'
+            }
+        ]
+    },
+    {
+        id: 3,
+        name: 'Chart #3',
+        intervalTime: 120,
+        position: {
+            x: 100,
+            y: 120
+        },
+        chartItems: [
+            {
+                id: 100,
+                name: 'Item #1'
+            },
+            {
+                id: 100,
+                name: 'Item #2'
+            },
+            {
+                id: 100,
+                name: 'Item #3'
+            },
+            {
+                id: 100,
+                name: 'Item #4'
+            }
+        ]
+    },
+    {
+        id: 4,
+        name: 'Chart #4',
+        intervalTime: 120,
+        position: {
+            x: 100,
+            y: 120
+        },
+        chartItems: [
+            {
+                id: 100,
+                name: 'Item #1'
+            },
+            {
+                id: 100,
+                name: 'Item #2'
+            },
+            {
+                id: 100,
+                name: 'Item #3'
+            },
+            {
+                id: 100,
+                name: 'Item #4'
+            }
+        ]
+    },
+    {
+        id: 5,
+        name: 'Chart #5',
+        intervalTime: 120,
+        position: {
+            x: 100,
+            y: 120
+        },
+        chartItems: [
+            {
+                id: 100,
+                name: 'Item #1'
+            },
+            {
+                id: 100,
+                name: 'Item #2'
+            },
+            {
+                id: 100,
+                name: 'Item #3'
+            },
+            {
+                id: 100,
+                name: 'Item #4'
+            }
+        ]
+    },
+    {
+        id: 6,
+        name: 'Chart #6',
+        intervalTime: 120,
+        position: {
+            x: 100,
+            y: 120
+        },
+        chartItems: [
+            {
+                id: 100,
+                name: 'Item #1'
+            },
+            {
+                id: 100,
+                name: 'Item #2'
+            },
+            {
+                id: 100,
+                name: 'Item #3'
+            },
+            {
+                id: 100,
+                name: 'Item #4'
+            }
+        ]
+    }
+]
